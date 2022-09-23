94826740b96bb2282a70dcbdec3b781cac947b91
Ajbar Imad <ajbar.imad.99@gmail.com>
2022-09-23 21:46:08 +0100

    create Experience section

--- components/Experience.js
+++ components/Experience.js
@@ -1,17 +1,9 @@
-import React from 'react';
+import React from 'react'
 import { motion } from 'framer-motion';
 
-//------------------------------------------------------
-
-function About() {
-
+function Experience() {
     return (
-        <motion.div
-            initial={{ opacity: 0 }}
-            whileInView={{ opacity: 1 }}
-            transition={{ duration: 1.5 }}
-            className='flex flex-col relative h-screen md:space-x-10 text-center md:text-left md:flex-row max-w-4xl mx-auto px-10 justify-evenly items-center'>
-
+        <div className='flex flex-col relative h-screen md:space-x-10 text-center md:text-left md:flex-row max-w-4xl mx-auto px-10 justify-evenly items-center'>
             <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-700 dark:text-gray-500 text-2xl'>
                 about
             </h3>
@@ -49,8 +41,8 @@ function About() {
                     master!
                 </p>
             </div>
-        </motion.div>
-    );
+        </div>
+    )
 }
 
-export default About
+export default Experience
