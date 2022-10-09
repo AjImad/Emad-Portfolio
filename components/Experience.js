65e561472168745759406ff7ee84feab3f6c8ba3
Ajbar Imad <ajbar.imad.99@gmail.com>
2022-10-09 17:39:42 +0100

    make ExperienceCard reusable component

--- components/Experience.js
+++ components/Experience.js
@@ -5,6 +5,44 @@ import ExperienceCard from './ExperienceCard';
 //------------------------------------------------------
 
 function Experience() {
+
+    const contentExperienceCard = [
+        {
+            companyPicture: '/images/pull4you.png',
+            titleJob: 'Front End Engineer',
+            duration: 'Jul 2022 - Present · 4 mos',
+            company: 'PULL4YOU SERVICES S.A.R.L · Internship',
+            tools: [
+                { imgSrc: '/images/skillsImgs/React.webp', title: 'React JS' },
+                { imgSrc: '/images/skillsImgs/typescript.svg', title: 'Typescript' },
+                { imgSrc: '/images/skillsImgs/redux.svg', title: 'Redux' },
+                { imgSrc: '/images/skillsImgs/mui.svg', title: 'Material UI' },
+                { imgSrc: '/images/skillsImgs/github.svg', title: 'Github' },
+                { imgSrc: '/images/skillsImgs/git.svg', title: 'Git' },
+                { imgSrc: '/images/skillsImgs/trello.svg', title: 'Trello' },
+            ],
+            description: {},
+        },
+        {
+            companyPicture: '/images/sie.jfif',
+            titleJob: 'Full-stack Developer - Stage PFE',
+            duration: 'May 2021 - Jun 2021 · 2 mos',
+            company: 'SIE Grupo · Internship',
+            tools: [
+                { imgSrc: '/images/skillsImgs/html.svg', title: 'HTML' },
+                { imgSrc: '/images/skillsImgs/css.svg', title: 'CSS' },
+                { imgSrc: '/images/skillsImgs/javascriptl.svg', title: 'Javascript' },
+                { imgSrc: '/images/skillsImgs/jquery.svg', title: 'JQuery' },
+                { imgSrc: '/images/skillsImgs/bootstrap.svg', title: 'Bootstrap' },
+                { imgSrc: '/images/skillsImgs/php.svg', title: 'PHP' },
+                { imgSrc: '/images/skillsImgs/github.svg', title: 'Github' },
+                { imgSrc: '/images/skillsImgs/git.svg', title: 'Git' },
+            ],
+            description: {},
+        },
+
+    ]
+
     return (
         <motion.div
             initial={{
@@ -22,8 +60,11 @@ function Experience() {
             </h3>
 
             <div className='w-full flex space-x-5 md:justify-center overflow-x-scroll p-10 snap-x snap-mandatory scrollbar scrollbar-thumb-[#c5ac8f] dark:scrollbar-thumb-orange-300 scrollbar-track-gray-400/20'>
-                <ExperienceCard />
-                <ExperienceCard />
+                {
+                    contentExperienceCard.map((content, index) => (
+                        <ExperienceCard ExperienceContent={content} key={index} />
+                    ))
+                }
             </div>
 
         </motion.div>
--- components/ExperienceCard.js
+++ components/ExperienceCard.js
@@ -4,9 +4,10 @@ import { motion } from 'framer-motion';
 
 //------------------------------------------------------
 
-function ExperienceCard() {
+function ExperienceCard({ ExperienceContent, key }) {
     return (
         <article
+            key={key}
             className='flex flex-col rounded-lg p-10 md:py-7 items-center space-y-2 flex-shrink-0 w-[400px] md:w-[450px] xl:w-[450px] snap-start bg-[#9c8c7a] shadow-lg shadow-[#9c8c7a] dark:shadow-inherit text-white dark:bg-[#292929]'
         >
             <motion.div
@@ -21,29 +22,28 @@ function ExperienceCard() {
                 transition={{
                     duration: 1.2
                 }}
+                className="w-10 h-10 rounded-full"
             >
                 <Image
-                    src="/images/fst.png"
+                    src={ExperienceContent.companyPicture}
                     alt="FST TANGER"
-                    width="167px"
-                    height="80px"
-                    className='rounded-full object-cover object-center'
+                    width="100%"
+                    height="100%"
+                    className='object-fit object-center'
                 />
             </motion.div>
 
             <div className='px-0 md:px-10'>
-                <h4 className='text-xl font-light'>Front End Engineer</h4>
-                <p className='font-medium text-sm mt-2'>PULL4YOU SERVICES S.A.R.L · Internship</p>
+                <h4 className='text-xl font-light'>{ExperienceContent.titleJob}</h4>
+                <p className='font-medium text-sm mt-2'>{ExperienceContent.company}</p>
                 <div className='flex flex-row space-x-3 my-2'>
-                    <img src='/images/skillsImgs/React.webp' title='React JS' className='w-7 h-7' />
-                    <img src='/images/skillsImgs/typescript.svg' title='Typescript' className='w-7 h-7' />
-                    <img src='/images/skillsImgs/redux.svg' title='Redux' className='w-7 h-7' />
-                    <img src='/images/skillsImgs/mui.svg' title='Material UI' className='w-7 h-7' />
-                    <img src='/images/skillsImgs/github.svg' title='Github' className='w-7 h-7' />
-                    <img src='/images/skillsImgs/git.svg' title='Git' className='w-7 h-7' />
-                    <img src='/images/skillsImgs/trello.svg' title='Trello' className='w-7 h-7' />
+                    {
+                        ExperienceContent.tools.map((image, index) => (
+                            <img src={image.imgSrc} title={image.title} className='w-7 h-7' />
+                        ))
+                    }
                 </div>
-                <p className='uppercase py-1 text-gray-200 dark:text-gray-500'>Jul 2022 - Present · 4 mos</p>
+                <p className='uppercase py-1 text-gray-200 dark:text-gray-500'>{ExperienceContent.duration}</p>
                 <ul className='list-disc space-y-4 ml-5 text-sm '>
                     <li>Summery Points</li>
                     <li>Summery Points</li>
