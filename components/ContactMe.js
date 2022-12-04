7555ca03c5fad72d6b544a2919295967958e623b
Ajbar Imad <ajbar.imad.99@gmail.com>
2022-12-04 15:24:35 +0100

    check email format required

--- components/ContactMe.js
+++ components/ContactMe.js
@@ -15,7 +15,7 @@ function ContactMe() {
     const schema = yup.object().shape({
         firstName: yup.string().required(),
         lastName: yup.string().required(),
-        email: yup.string().required(),
+        email: yup.string().required().email(),
         message: yup.string(),
     })
 
