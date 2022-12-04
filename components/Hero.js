4499df653fd9344033ff29e87bc4afc673daecc6
Ajbar Imad <ajbar.imad.99@gmail.com>
2022-12-04 15:08:05 +0100

    change class to className

--- components/Hero.js
+++ components/Hero.js
@@ -87,7 +87,7 @@ function Hero() {
             <div className='relative w-[100%]'>
 
                 <div className='absolute top-24 md:top-14 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
-                    <button class="px-6 py-2 rounded-full border border-[#88ccca] text-[#88ccca]
+                    <button className="px-6 py-2 rounded-full border border-[#88ccca] text-[#88ccca]
                     hover:border-[#79B4B7] hover:text-[#79B4B7] dark:border-orange-300/60 dark:text-orange-300/60 uppercase
                     shadow-md dark:hover:border-orange-200 dark:hover:text-orange-200 hover:shadow-lg transition duration-150 ease-in-out
                     ">
@@ -100,7 +100,7 @@ function Hero() {
                         >
                             My Resume
                             <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="download"
-                                class="w-3 ml-2" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                                className="w-3 ml-2" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
                                 <path fill="currentColor"
                                     d="M216 0h80c13.3 0 24 10.7 24 24v168h87.7c17.8 0 26.7 21.5 14.1 34.1L269.7 378.3c-7.5 7.5-19.8 7.5-27.3 0L90.1 226.1c-12.6-12.6-3.7-34.1 14.1-34.1H192V24c0-13.3 10.7-24 24-24zm296 376v112c0 13.3-10.7 24-24 24H24c-13.3 0-24-10.7-24-24V376c0-13.3 10.7-24 24-24h146.7l49 49c20.1 20.1 52.5 20.1 72.6 0l49-49H488c13.3 0 24 10.7 24 24zm-124 88c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20zm64 0c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20z">
                                 </path>
@@ -109,7 +109,7 @@ function Hero() {
                     </button>
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
 
